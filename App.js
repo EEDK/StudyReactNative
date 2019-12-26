c04447f30f5dcbd35b756a05f646ea60f2a741ae
kimDongEon <kde9604@naver.com>
2019-12-26 17:32:22 +0900

    new

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
+      <View style={styles.yellowView} />
+      <View style={styles.blueView} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
-  text:{
-    color: 'black'
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 3,
+    backgroundColor: "blue"
   }
 });
