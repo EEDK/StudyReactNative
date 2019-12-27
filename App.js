bb1cd43857b650cee60a58c4834a78f35970696b
kimDongEon <kde9604@naver.com>
2019-12-27 13:56:47 +0900

    third change

--- App.js
+++ App.js
@@ -1,25 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView} />
-      <View style={styles.blueView} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueView: {
-    flex: 3,
-    backgroundColor: "blue"
+export default class extends React.Component {
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  };
+  componentDidMount() {
+    this.getLocation();
   }
-});
+  render() {
+    return <Loading />;
+  }
+}
