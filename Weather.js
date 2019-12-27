2b2ebeab490569d172d05a01f56e6cd1afe831a2
kimDongEon <kde9604@naver.com>
2019-12-27 23:27:35 +0900

    fianl change

--- Weather.js
+++ Weather.js
@@ -7,39 +7,57 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 const weatherOptions = {
   Thunderstorm: {
     iconName: "weather-lightning",
-    gradient: ["#373B44", "#4286f4"]
+    gradient: ["#373B44", "#4286f4"],
+    title:"태풍이 불고있네요",
+    subtitle:"집안에만 있으세요"
   },
   Drizzle: {
     iconName: "weather-hail",
-    gradient: ["#89F7FE", "#66A6FF"]
+    gradient: ["#89F7FE", "#66A6FF"],
+    title:"이슬비가 내리고있습니다.",
+    subtitle:"집안에만 있으세요"
   },
   Rain: {
     iconName: "weather-rainy",
-    gradient: ["#00C6FB", "#005BEA"]
+    gradient: ["#00C6FB", "#005BEA"],
+    title:"비가 오고있네요",
+    subtitle:"집안에만 있으세요"
   },
   Snow: {
     iconName: "weather-snowy",
-    gradient: ["#7DE2FC", "#B9B6E5"]
+    gradient: ["#7DE2FC", "#B9B6E5"],
+    title:"눈이 내리고 있네요",
+    subtitle:"나가실려면 나가세요"
   },
   Atmosphere: {
     iconName: "weather-hail",
-    gradient: ["#89F7FE", "#66A6FF"]
+    gradient: ["#89F7FE", "#66A6FF"],
+    title:"밖이 엉망입니다",
+    subtitle:"집안에만 있으세요"
   },
   Clear: {
     iconName: "weather-sunny",
-    gradient: ["#FF7300", "#FEF253"]
+    gradient: ["#FF7300", "#FEF253"],
+    title:"날씨가 좋네요",
+    subtitle:"밖에 나가보시는게 어떄요?"
   },
   Clouds: {
     iconName: "weather-cloudy",
-    gradient: ["#D7D2CC", "#304352"]
+    gradient: ["#D7D2CC", "#304352"],
+    title:"구름이 꼈습니다.",
+    subtitle:"나가실려면 나가세요"
   },
   Mist: {
     iconName: "weather-hail",
-    gradient: ["#4DA0B0", "#D39D38"]
+    gradient: ["#4DA0B0", "#D39D38"],
+    title:"안개가 꼈네요",
+    subtitle:"외출할때 조심하세요"
   },
   Dust: {
     iconName: "weather-hail",
-    gradient: ["#4DA0B0", "#D39D38"]
+    gradient: ["#4DA0B0", "#D39D38"],
+    title:"미세먼지가 심합니다",
+    subtitle:"집안에만 있으세요"
   },
   Haze: {
     iconName: "weather-hail",
@@ -50,25 +68,47 @@ const weatherOptions = {
 };
 
 
-export default function Weather({ temp , condition }) {
+export default function Weather({ temp, condition }) {
   return (
-      <LinearGradient
-        colors={weatherOptions[condition].gradient}
-        style={styles.container}
-      >
-        <StatusBar barStyle="light-content"/>
-      <View style = {styles.halfcontainer}>
-          <MaterialCommunityIcons 
-            size = {96}
-            name={weatherOptions[condition].iconName}
-            color = "white"
-          />
-          <Text style={styles.temp}>{temp}</Text>
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions[condition].iconName}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}°</Text>
       </View>
-      <View style = {styles.halfcontainer}></View>
-      </LinearGradient>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
   );
 }
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust"
+  ]).isRequired
+};
+
 
 Weather.propTypes = {
     temp: PropTypes.number.isRequired,
@@ -87,18 +127,35 @@ Weather.propTypes = {
   };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-    },
-    temp: {
-        fontSize : 42,
-        color : "white"
-    },
-    halfcontainer:{
-        flex:1,
-        justifyContent : "center",
-        alignItems: "center"
-    }
-  });
+  container: {
+    flex: 1
+  },
+  temp: {
+    fontSize: 42,
+    color: "white"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+    textAlign: "left"
+  },
+  subtitle: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: 24,
+    textAlign: "left"
+  },
+  textContainer: {
+    alignItems: "flex-start",
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    flex: 1
+  }
+});
